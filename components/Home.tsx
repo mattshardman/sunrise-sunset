1b497e434af3395e7105e46e72b4a6a3b6ca43d7
mattshardman <mattshardman@gmail.com>
2020-01-20 15:59:20 +0000

    add title

--- components/Home.tsx
+++ components/Home.tsx
@@ -4,6 +4,7 @@ import MainPanel from './MainPanel';
 import Layout from './Layout';
 import SunTimes from './SunTimes';
 import SunArc from './SunArc';
+import Title from './Title';
 
 function Home() {
     const timeData = useTimes();
@@ -12,6 +13,7 @@ function Home() {
 
     return (
         <Layout>
+            <Title text="Sun Times" />
             <MainPanel>
                 <SunArc sunPosition={sunPosition} />
                 <SunTimes {...timeData} />
