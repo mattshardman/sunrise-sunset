0a2759b689e7f41a1eeb7a7e83b588cb30774a75
mattshardman <mattshardman@gmail.com>
2020-01-20 16:00:15 +0000

    abstract out functions to separate file

--- components/useTimes.tsx
+++ components/useTimes.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import * as utils from './utils';
 
 interface Times {
     sunrise?: number;
@@ -12,49 +12,12 @@ function useTimes() {
     const [times, setTimes]: [Times, Function] = useState({ });
     const [sunPosition, setSunPosition] = useState();
 
-    function getTime(timeNow, inputTime) {
-        const now = timeNow.toDateString();
-        return new Date(`${now} ${inputTime}`);
-    }
-    
-    function getTimeNow() {
-        return new Date(Date.now());
-    }
-    
-    function getPosition(results) {
-        const now = getTimeNow();
-        const sunSet = getTime(now, results.sunset);
-        const sunRise = getTime(now, results.sunrise);
-
-        const fullDay = sunSet.valueOf() - sunRise.valueOf();
-        const untilNow = now.valueOf() - sunRise.valueOf();
-        return 180 * untilNow / fullDay;
-    }
-
-    const getLocation = async () : Promise<Coordinates> => {
-        return new Promise((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const { coords } = position;
-                resolve(coords);
-            }, (error) => {
-                reject(error);
-            });
-        });
-    }
-
-    const getSunTimeData = async (coords: Coordinates) => {
-        const { latitude, longitude } = coords;
-        const response = await axios.post("api/times", { latitude, longitude });
-        const { sunrise, sunset } : { sunset: number, sunrise: number } = response.data;
-        return { sunrise, sunset };
-    };
-
     const getTimes = async () => {
         setLoading(true);
         try {
-            const coords = await getLocation();
-            const sunTimes = await getSunTimeData(coords);
-            const position = getPosition(sunTimes);
+            const coords = await utils.getLocation();
+            const sunTimes = await utils.getSunTimeData(coords);
+            const position = utils.getPosition(sunTimes);
 
             setSunPosition(position);
             setTimes(sunTimes);
@@ -63,7 +26,6 @@ function useTimes() {
             setLoading(false);
             setError(true);
         }
-       
     }
 
     return { sunPosition, times, getTimes, loading, error };
