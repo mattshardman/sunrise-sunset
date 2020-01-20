f6084c58cb439c435f0ba01c92119d0865c23b87
mattshardman <mattshardman@gmail.com>
2020-01-20 14:03:03 +0000

    add get times function to get sunrise and sunset times when browser completes getting location

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Times {
+    sunrise?: number;
+    sunset?: number;
+}
 
 function Home() {
-    const [coordinates, setCoordinates] : [Coordinates, Function] = useState();
+    const [coordinates, setCoordinates]: [Coordinates, Function] = useState();
+    const [times, setTimes] : [Times, Function] = useState({});
 
     const getLocation = () => {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -10,11 +17,28 @@ function Home() {
         });
     }
 
-    console.log(coordinates);
+    const getTimes = async () => {
+        try {
+            const { latitude, longitude } = coordinates;
+            const response = await axios.post("api/times", { latitude, longitude });
+            const { sunrise, sunset } = response.data;
+            setTimes({ sunrise, sunset });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    console.log(times);
+
+    useEffect(() => {
+        if (coordinates) {
+            getTimes();
+        }
+    }, [coordinates]);
 
     return (
         <main>
-            <button onClick={getLocation}>Location</button>
+            <button onClick={getLocation}>Get Location</button>
         </main>
     )
 }
